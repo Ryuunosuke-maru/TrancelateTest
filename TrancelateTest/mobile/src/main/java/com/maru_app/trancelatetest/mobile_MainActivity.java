b25a21c27f1f158e436c9a3177c6262bfb093976
maru <s13170@tokyo.kosen-ac.jp>
2017-05-19 13:04:20 +0900

    初回起動(build)

---
+++ TrancelateTest/mobile/src/main/java/com/maru_app/trancelatetest/mobile_MainActivity.java
@@ -0,0 +1,13 @@
+package com.maru_app.trancelatetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mobile_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobile__main);
+    }
+}
